489a75ab5e1ca9cedb2d4417f5a5e36a93bcbae6
GabrielGomesBahia <gabrielgbahia@gmail.com>
2023-04-15 09:41:38 -0300

    teste

---
+++ phpteste.php
@@ -0,0 +1,14 @@
+<?
+
+$array = [1 ,5, 10 ,25 ,69 ,2 ,4];
+
+foreach($array as $a) {
+    if( $a > 5 ) {
+        echo $a . "\n";
+    } else {
+        echo "0" . "\n";
+    }
+}
+
+$teste = 0;
+$teste2 = 1;
