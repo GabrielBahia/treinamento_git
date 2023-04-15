264734e34079af8ea5e881ab38ceda978f29a6e7
Gabriel Gomes Bahia <92956450+GabrielBahia@users.noreply.github.com>
2023-04-15 09:49:08 -0300

    Update phpteste.php

--- phpteste.php
+++ phpteste.php
@@ -3,14 +3,10 @@
 $array = [1 ,5, 10 ,25 ,69 ,2 ,4];
 
 $cont = 0;
-while(true) {
-    $cont++;
-    if($cont == 10) {
-        break;
-    } else {
-        echo $cont . "\n";
-    }
+foreach($array as $a) {
+    if($a > 5) {
+        echo $a 
+        }
 }
-
 $teste = 0;
 $teste2 = 1;
